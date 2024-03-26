5b0671a75c02611df4c9e75791fedeca63e36bb2
pheonix8 <61965711+pheonix8@users.noreply.github.com>
2024-03-26 17:08:42 +0100

    added conda-setup-script.sh

---
+++ admin/conda-setup-script.sh
@@ -0,0 +1,8 @@
+conda create --name aiap-miniproject-2 python=3.11 -y
+conda activate aiap-miniproject-2
+conda install -c conda-forge notebook -y
+conda install -c conda-forge tensorflow -y
+conda install -c conda-forge numpy -y
+conda install -c conda-forge pandas -y
+conda install -c conda-forge matplotlib -y
+conda install -c conda-forge scikit-learn -y
