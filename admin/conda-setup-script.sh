6c950ef1861813f56a1d6b0ac587689bdd94c9d2
vho129554 <vho129554@stud.gibb.ch>
2024-03-30 15:30:17 +0100

    Make setup script wait for ENTER before closing

--- admin/conda-setup-script.sh
+++ admin/conda-setup-script.sh
@@ -6,3 +6,6 @@ conda install -c conda-forge numpy -y
 conda install -c conda-forge pandas -y
 conda install -c conda-forge matplotlib -y
 conda install -c conda-forge scikit-learn -y
+
+echo "Press ENTER to continue..."
+read -r
